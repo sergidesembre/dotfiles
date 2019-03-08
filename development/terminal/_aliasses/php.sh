968d5de6e57805a8221cb48a1d7eaa14fac76b2c
Sergi Desembre <sergi.desembre@gmail.com>
2019-03-08 12:45:23 +0100

    Create shell files to register aliasses and exports
    I create aliasses to call composer vendors and set export vars to define correct path

---
+++ development/terminal/_aliasses/php.sh
@@ -0,0 +1,4 @@
+alias phplint='~/.composer/vendor/bin/parallel-lint --colors'
+alias phpunit='~/.composer/vendor/bin/phpunit --colors'
+alias phpsniffer='~/.composer/vendor/bin/phpcs --colors -p'
+alias phpstan='~/.composer/vendor/bin/phpstan analyse'
