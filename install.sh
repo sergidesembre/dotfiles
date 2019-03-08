a83f8fac4abb5522b9d58a43e8209444a0e24a3e
Sergi Desembre <sergi.desembre@gmail.com>
2019-03-08 12:52:22 +0100

    Create main install.sh file to execute all
    This shell call the other install.sh file to execute, for the moment only have development

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DOTFILES_PATH=$HOME/.dotfiles
+
+#---------------------#
+#      LANGUAGES      #
+#---------------------#
+
+echo 'Installing language development tools...';
+source ${DOTFILES_PATH}/development/install.sh
