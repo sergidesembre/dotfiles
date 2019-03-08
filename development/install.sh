942a2b1956a57dbc6d8fff720913ce42c8e5756a
Sergi Desembre <sergi.desembre@gmail.com>
2019-03-08 12:49:28 +0100

    Create install.sh for development config
    I create a shell script file to automatize and install all config about development

---
+++ development/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+DOTFILES_DEVELOPMENT_PATH=$HOME/.dotfiles/development
+
+__checkEmptyFolder()
+{
+    if [ "$(ls -A ${DOTFILES_DEVELOPMENT_PATH}/$1)" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# PHP
+mkdir -p $HOME/.composer
+ln -sf ${DOTFILES_DEVELOPMENT_PATH}/languages/php/composer.json $HOME/.composer
+
+# Register exposrts
+__checkEmptyFolder terminal/_exports
+if [ "$?" -eq "0" ]; then
+    for exportsToRegister in ${DOTFILES_DEVELOPMENT_PATH}/terminal/_exports/*; do
+        source $exportsToRegister;
+    done
+fi
+
+# Register aliasses
+__checkEmptyFolder terminal/_aliasses
+if [ "$?" -eq "0" ]; then
+    for aliassesToRegister in ${DOTFILES_DEVELOPMENT_PATH}/terminal/_aliasses/*; do
+        source $aliassesToRegister;
+    done
+fi
