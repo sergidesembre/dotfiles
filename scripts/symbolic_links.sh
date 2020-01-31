c9e87258752007e5df7281022e387340979e2da6
Sergi Desembre <sergi.desembre@gmail.com>
2020-01-31 19:20:50 +0100

    Update and rename symbolic links files

--- scripts/symbolic_links.sh
+++ scripts/symbolic_links.sh
@@ -6,3 +6,11 @@ ln -sf $HOME/.dotfiles/git/.gitignore_global $HOME
 ln -sf $HOME/.dotfiles/git/.gitconfig $HOME
 
 ln -sf $HOME/.dotfiles/terminal/zsh/.init_aliases $HOME/.config/zsh
+
+#---------------------#
+#        GIT          #
+#---------------------#
+
+ln -sf ${DOTFILES_GIT_PATH}/.gitmessage $HOME
+ln -sf ${DOTFILES_GIT_PATH}/.gitignore_global $HOME
+ln -sf ${DOTFILES_GIT_PATH}/.gitconfig $HOME
